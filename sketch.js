6ab833b34e07a4cee99a513dd0c1d5073c19ce89
HippoFalcon <277467@bwls.net>
2016-10-13 17:06:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-
+  background(0,0,0);
 }
 
 function draw() {
+  var x = mouseX;
+  var y = mouseY;
+  //Rectangular Body
+  fill(198, 196, 192)
+  rect(x, 200, 100, 20);
+  rect(x + 15, 178, 70, 40);
 
 }
