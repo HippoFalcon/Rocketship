2c2476d7c642d9b3e3b532d64a0a9030a46f162e
HippoFalcon <277467@bwls.net>
2016-10-13 17:11:21 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  background(0,0,0);
 }
 
 function draw() {
+  background(0,0,0);
   var x = mouseX;
   var y = mouseY;
   //Rectangular Body
   fill(198, 196, 192)
+  noStroke()
   rect(x, y, 45, 100);
+  
+  //triangular cone piece
 
 }
