d85a161bc96bf57456027f0dfc774023498824e7
HippoFalcon <277467@bwls.net>
2016-10-13 17:31:55 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,16 +16,4 @@ function draw() {
   //triangular cone piece
   fill(249, 242, 14)
   triangle(x,y,x-22.5,y+25,x+45,y);
-  
-  //light?
-  size(100, 100, P3D);
-  background(0);
-  noStroke();
-  // Sets the default ambient 
-  // and directional light
-  lights();
-  translate(20, 50, 0);
-  sphere(30);
-  translate(60, 0, 0);
-  sphere(30);
 }
